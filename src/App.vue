1c0770ae95eb6aea1f0454adaea528d55601a356
Agustín Rodríguez <killgt@gmail.com>
2017-11-26 19:01:07 +0100

    responsive fixes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app" class="flex flex-col min-h-screen border-t-2 border-teal">
 
         <router-link to="/" class="container mx-auto my-8 flex flex-col justify-center items-center font-sans">
-            <h1 class="text-5xl text-teal-dark font-thin tracking-wide">Tailwind<span class="text-orange">Components</span></h1>
+            <h1 class="text-4xl sm:text-5xl text-teal-dark font-thin tracking-tight sm:tracking-wide">Tailwind<span class="text-orange">Components</span></h1>
             <p class="text-sm text-grey-dark tracking-wide">A repository for community components</p>
         </router-link>
 
@@ -11,11 +11,11 @@
             <router-view />
         </div>
 
-        <router-link to="/new" class="absolute pin-t pin-r m-4 bg-transparent hover:bg-orange text-teal-dark font-semibold hover:text-white py-2 px-4 border border-teal hover:border-transparent rounded">
+        <router-link to="/new" class="hidden sm:block absolute pin-t pin-r m-4 bg-transparent hover:bg-orange text-teal-dark font-semibold hover:text-white py-2 px-4 border border-teal hover:border-transparent rounded">
             Submit your own
         </router-link>
 
-        <div class="text-center text-grey text-xs mt-auto mb-3">
+        <div class="text-center text-grey text-xs mt-auto mb-3 px-3">
             <p>
                 Made with ❤️ by <a class="text-grey-darker" href="https://twitter.com/killgt">@killgt</a> @ Málaga, Spain &bull;
                 Tailwind is a Utility Framework by
